b1e239f35de9ef98e807a9890e0282d9978439a3
sara pulido <sara01rizo@gmail.com>
2023-01-30 00:25:21 +0100

    Set up all libs and start with sig up form as module

---
+++ pages/sign_up.tsx
@@ -0,0 +1,30 @@
+import { Input } from "react-daisyui";
+
+export default function SignUpPage () {
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexFlow: "column",
+                gap: 15,
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100vh"
+            }}
+        >
+            SignUpForm Here
+
+            <div className="form-control w-full max-w-xs">
+                <label htmlFor={'email'} className="label">
+                    <span className="label-text">email</span>
+                </label>
+                    
+                <Input 
+                    color={"ghost"}
+                    id={"email"}
+                    type={"text"}
+                />
+            </div>
+        </div>
+    )
+}
