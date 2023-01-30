4485f1ee9007766f50851c0488e276fcc5f47210
sara pulido <sara01rizo@gmail.com>
2023-01-30 02:09:35 +0100

    Elegant  about separation of concerns, add text field file

--- pages/sign_up.tsx
+++ pages/sign_up.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useCallback } from "react";
 import { Input } from "react-daisyui";
 import { useForm } from "react-hook-form";
+import { TextField } from "../src/components/TextField";
 
 export default function SignUpPage () {
 
@@ -23,55 +24,30 @@ export default function SignUpPage () {
                 height: "100vh"
             }}
             onSubmit={handleSubmit(onValid)}
-        >
+        >         
 
-            <div className="form-control w-full max-w-xs">
-                <label htmlFor={'email'} className="label">
-                    <span className="label-text">email</span>
-                </label>
-                    
-                <Input 
-                    color={"ghost"}
-                    id={"email"}
-                    type={"text"}
-                    { ... register("email", { required: "Please type your email here" })}
-                />
-                {
-                    errors.email?.message ? (<span className="label-text text-error">{errors.email?.message as string}</span>) : null
-                }
-            </div>
+            <TextField 
+                id='email'
+                label='email'
+                inputProps={register("email", {required: "Please type your email" })}
+                error={errors.email?.message as string}
+            />
 
-            <div className="form-control w-full max-w-xs">
-                <label htmlFor={'password'} className="label">
-                    <span className="label-text">password</span>
-                </label>
-                    
-                <Input 
-                    color={"ghost"}
-                    id={"password"}
-                    type={"password"}
-                    { ... register("password", { required: "Please type your password here" })}
-                />
-                {
-                    errors.password?.message ? (<span className="label-text text-error">{errors.password?.message as string}</span>) : null
-                }
-            </div>
+            <TextField 
+                id='password'
+                label='password'
+                type='password'
+                inputProps={register("password", {required: "Please type your password" })}
+                error={errors.password?.message as string}
+            />
 
-            <div className="form-control w-full max-w-xs">
-                <label htmlFor={'confirm password'} className="label">
-                    <span className="label-text">confirm password</span>
-                </label>
-                    
-                <Input 
-                    color={"ghost"}
-                    id={"confirm password"}
-                    type={"password"}
-                    { ... register("confirm password", { required: "Please confirm your password here" })}
-                />
-                {
-                    errors.confirmPassword?.message ? (<span className="label-text text-error">{errors.confirmPassword?.message as string}</span>) : null
-                }
-            </div>
+            <TextField 
+                id='confirm-password'
+                label='confirm-password'
+                type='password'
+                inputProps={register("confirmPassword", {required: "Please tyoe your email" })}
+                error={errors.confirmPassword?.message as string}
+            />
 
             <button>Submit</button>
         </form>
