65fc421fa080164cfa43ccf3724e96002adc0667
sara pulido <sara01rizo@gmail.com>
2023-01-31 15:11:57 +0100

    Update: separation of concenrs, about handle submit data

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,73 +1,13 @@
 import React from "react";
-import { useCallback } from "react";
-import { useForm } from "react-hook-form";
-import { TextField } from "../src/components/TextField";
-import * as z from 'zod';
-import {zodResolver} from '@hookform/resolvers/zod';
-
-const SignupSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6).max(24),
-    confirmPassword: z.string().min(6).max(24),
-    strip: z.any(),
-}). refine(
-    (form) => {
-        return form.confirmPassword === form.password
-    },
-    {
-        message: "Password must match",
-        path: ["confirmPassword"]
-    }
-)
+import { SignUpForm } from "../src/SignUpForm/SignUpForm";
 
 export default function SignUpPage () {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm({
-        resolver: zodResolver(SignupSchema),
-    });
-    const onValid = useCallback((data: unknown) => {
-        console.log(`onValid`, data)
-    }, [])
-
-    console.log('Render sign-up runing function', errors)
-    
     return (
-        <form
-            style={{
-                display: "flex",
-                flexFlow: "column",
-                gap: 15,
-                alignItems: "center",
-                justifyContent: "center",
-                height: "100vh"
-            }}
-            onSubmit={handleSubmit(onValid)}
-        >         
-
-            <TextField 
-                id='email'
-                label='email'
-                inputProps={register("email")}
-                error={errors.email?.message as string}
-            />
-
-            <TextField 
-                id='password'
-                label='password'
-                type='password'
-                inputProps={register("password")}
-                error={errors.password?.message as string}
-            />
-
-            <TextField 
-                id='confirm-password'
-                label='confirm-password'
-                type='password'
-                inputProps={register("confirmPassword")}
-                error={errors.confirmPassword?.message as string}
-            />
-
-            <button>Submit</button>
-        </form>
+        <SignUpForm 
+            onSubmitReady={(data) => {
+            console.log("handle submit ready data", data)
+        }}
+        />
     )
 }
---
+++ src/SignUpForm/SignUpForm.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { TextField } from "../components/TextField";
+import * as z from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod';
+
+const SignupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6).max(24),
+    confirmPassword: z.string().min(6).max(24),
+    strip: z.any(),
+}). refine(
+    (form) => {
+        return form.confirmPassword === form.password
+    },
+    {
+        message: "Password must match",
+        path: ["confirmPassword"]
+    }
+)
+
+interface SignUpFormProps {
+    onSubmitReady: (data: unknown) => void
+}
+
+export const SignUpForm = (props: SignUpFormProps ) => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: zodResolver(SignupSchema),
+    });
+   
+    return (
+        <form
+            style={{
+                display: "flex",
+                flexFlow: "column",
+                gap: 15,
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100vh"
+            }}
+            onSubmit={handleSubmit(props.onSubmitReady)}
+        >         
+            <h2>Sign Up 🧭</h2>
+            <TextField 
+                id='email'
+                label='email'
+                inputProps={register("email")}
+                error={errors.email?.message as string}
+            />
+
+            <TextField 
+                id='password'
+                label='password'
+                type='password'
+                inputProps={register("password")}
+                error={errors.password?.message as string}
+            />
+
+            <TextField 
+                id='confirm-password'
+                label='confirm-password'
+                type='password'
+                inputProps={register("confirmPassword")}
+                error={errors.confirmPassword?.message as string}
+            />
+
+            <button>Submit</button>
+        </form>
+    )
+}
